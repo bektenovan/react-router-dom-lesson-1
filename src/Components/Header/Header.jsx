3ac043324612eb35a7351e9c342559c879c47576
bektenovan <bektenovan.01@gmail.com>
2022-05-23 20:49:26 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+
+//         <Navbar.Brand className='btn btn-success' href="#home">Home</Navbar.Brand>
+
+const Header = () => {
+    const location = useLocation()
+    // console.log(location);
+    return (
+        <Navbar>
+            <Container>
+                <Link to='/'>
+                    <Navbar.Brand className={location.pathname === '/' ? "btn btn-success" : "btn btn-light"} href="#home">Home</Navbar.Brand>
+                </Link>
+                <Link to='/add'>
+                    <Navbar.Brand className={location.pathname === '/add' ? "btn btn-success" : "btn btn-light"} href="#home">Add</Navbar.Brand>
+                </Link><Link to='/edit'>
+                    <Navbar.Brand className={location.pathname === '/edit' ? "btn btn-success" : "btn btn-light"} href="#home">Edit</Navbar.Brand>
+                </Link>
+                <Link to='/contacts'>
+                    <Navbar.Brand className={location.pathname === '/contacts' ? "btn btn-success" : "btn btn-light"} href="#home">Contacts</Navbar.Brand>
+                </Link>
+
+
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        Signed in as: <a href="#login">Mark Otto</a>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
